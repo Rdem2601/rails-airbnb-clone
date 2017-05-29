e39383b9d6689f4e90e152723e18160442c16da9
Romain Demoustier <rdemoustier@hotmail.com>
2017-05-29 15:10:39 +0200

    done

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170529124746) do
+ActiveRecord::Schema.define(version: 20170529130455) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,16 @@ ActiveRecord::Schema.define(version: 20170529124746) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_items_on_user_id"
+  end
+
+  create_table "reviews", force: :cascade do |t|
+    t.string "content"
+    t.bigint "user_id"
+    t.bigint "item_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["item_id"], name: "index_reviews_on_item_id"
+    t.index ["user_id"], name: "index_reviews_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -44,4 +54,6 @@ ActiveRecord::Schema.define(version: 20170529124746) do
   end
 
   add_foreign_key "items", "users"
+  add_foreign_key "reviews", "items"
+  add_foreign_key "reviews", "users"
 end
