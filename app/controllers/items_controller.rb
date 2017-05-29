5845013247637a4fcb95e5ed5ee39d40b0bdc3e0
Romain Demoustier <rdemoustier@hotmail.com>
2017-05-29 17:25:39 +0200

    filled in items controller, created view/items/index

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,2 +1,39 @@
 class ItemsController < ApplicationController
+
+before_action :find_item, only: [:show, :destroy]
+
+  def index
+    @items = Items.all
+  end
+
+  def new
+    @item = Item.new
+  end
+
+  def show
+  end
+
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to item_path(@item)
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    @item.destroy
+    redirect_to item_path
+  end
+
+  private
+
+  def find_item
+    @item = Item.find(params[:id])
+  end
+
+  def item_params
+    params.require(:item).permit(:description, :price)
+  end
 end
