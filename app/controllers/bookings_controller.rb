b799b971b74037a0f467d321f9a457070c1c0eb7
Romain Demoustier <rdemoustier@hotmail.com>
2017-05-30 14:10:05 +0200

    done lots of features

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,2 +1,28 @@
 class BookingsController < ApplicationController
+
+  def new
+    @booking = Booking.new
+    @item = Item.find(params[:item_id])
+  end
+
+  def show
+    @booking = Booking.find(params[:id])
+  end
+
+  def create
+    @booking = Booking.new(booking_params)
+    @booking.user = current_user
+    @booking.item = Item.find(params[:item_id])
+    @booking.date = DateTime.now.to_date
+    if @booking.save
+      redirect_to item_booking_path(@booking.item, @booking)
+    else
+      render :new
+    end
+  end
+
+  private
+  def booking_params
+    params.require(:booking).permit(:number_of_days)
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,19 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170530085605) do
+ActiveRecord::Schema.define(version: 20170530112359) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "bookings", force: :cascade do |t|
-    t.string "start_date"
-    t.string "end_date"
     t.string "amount_paid"
     t.bigint "user_id"
     t.bigint "item_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "number_of_days"
+    t.string "date"
     t.index ["item_id"], name: "index_bookings_on_item_id"
     t.index ["user_id"], name: "index_bookings_on_user_id"
   end
