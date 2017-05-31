1cfee063ea3ef4badafc252366e884431ee575d1
Romain Demoustier <rdemoustier@hotmail.com>
2017-05-31 11:41:46 +0200

    forms bootstraped, photo user added

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,4 +4,19 @@ class UsersController < ApplicationController
     @user = current_user
     @bookings = Booking.where(user_id: @user.id)
   end
+
+  def update
+    @user = current_user
+    if @user.update(user_params)
+      redirect_to user_path(@user)
+    else
+      render :show
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:photo, :photo_cache)
+  end
 end
