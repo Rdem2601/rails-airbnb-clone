f45f1657a8f8b17b096dae80629ee121b5967021
Romain Demoustier <rdemoustier@hotmail.com>
2017-06-02 09:27:23 +0200

    dependent destroy

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,6 +1,7 @@
 class Item < ApplicationRecord
   belongs_to :user
   has_many :bookings, dependent: :destroy
+  has_many :reviews, dependent: :destroy
   validates :description, presence: true
   validates :price, presence: true
   mount_uploader :photo, PhotoUploader
